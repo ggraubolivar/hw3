716c6321e5103178c5c0e14687493bd0634e5b11
Gustavo "Goose" Grau Bolivar <gustavo.graubolivar@kellogg.northwestern.edu>
2023-05-19 16:47:22 +0000

    Almost there again,good god.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,6 +10,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
+    @posts = Post.where({ "place_id" => @place["id"] })
   end
 
   def create
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+  
   def show
-    @place = Place.find_by({ "id" => params["place"]["id"] })
+    @place = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({ "place_id" => @place["id"] })
   end
 

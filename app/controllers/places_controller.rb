38ac58166976014ff282ad148d124f9186dee66e
Gustavo "Goose" Grau Bolivar <gustavo.graubolivar@kellogg.northwestern.edu>
2023-05-19 05:47:31 +0000

    Trying new places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,24 @@
 class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    # start with a new Place
+    @place = Place.new
+
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["name"]
+    
+    # save Contact row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
+  
 end
